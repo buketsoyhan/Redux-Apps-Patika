2eab79df20ca229659bbe53cdbde4fdf3c00beea
Buket Soyhan <buketsoyhan1998@outlook.com>
2022-11-12 21:58:21 +0300

    Todo app created

---
+++ todo-app/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div>
+            <footer className="info">
+                <p>Click to edit a todo</p>
+                <p>Created by <a href="https://www.linkedin.com/in/buketsoyhan/">Buket Soyhan</a></p>
+                <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
